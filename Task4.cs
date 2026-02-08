787616ccff2710f77bca1777549f475bc10b0d55
Алишер Шумекеев <floyllx@gmail.com>
2026-02-08 18:09:49 +0500

    Add files via upload

---
+++ Task4.cs
@@ -0,0 +1,38 @@
+﻿public interface INotificationSender
+{ 
+    void Send(string message);
+}
+
+public class EmailSender : INotificationSender
+{
+    public void Send(string message)
+    {
+        Console.WriteLine("Email sent: " + message);
+    }
+}
+
+public class SmsSender : INotificationSender
+{
+    public void Send(string message)
+    {
+        Console.WriteLine("SMS sent: " + message);
+    }
+}
+
+public class NotificationService
+{
+    private INotificationSender emailSender;
+    private INotificationSender smsSender;
+
+    public NotificationService(INotificationSender emailSender, INotificationSender smsSender)
+    {
+        this.emailSender = emailSender;
+        this.smsSender = smsSender;
+    }
+
+    public void SendNotification(string message)
+    {
+        emailSender.Send(message);
+        smsSender.Send(message);
+    }
+}
